d2a6ca4de9030137ae3358bb5b20a29d8f4059f9
0xC0ffee1 <samthrw@proton.me>
2024-02-07 22:38:58 +0200

    Update config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://minevalley-docs.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/minevalley-docs',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
